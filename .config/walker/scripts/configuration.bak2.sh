23254718fa8d00e868ff5533d167ea1ba2d9400d
NEO-CORTE <pataponlaw@gmail.com>
2025-11-19 21:09:16 +0100

    Removed redundant scripts
    New configuration.sh is better

--- .config/walker/scripts/configuration.bak2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-choice="$(find ~/.dotfiles/.config/ -maxdepth 1 -mindepth 1 -type d | sort -n | walker --dmenu -p "Configuration: ")"
-[ -n "${choice}" ] && kitty nvim -c "cd ${choice}"
